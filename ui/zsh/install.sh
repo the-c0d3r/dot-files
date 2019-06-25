ad6123ada293e0f445e8aa4f835b974f0a15bbae
the-c0d3r <the-c0d3r@users.noreply.github.com>
2019-06-25 14:42:42 +0800

    ui/zsh/install.sh: 755 perm, removed autojump as it messes up the servers

--- ui/zsh/install.sh
+++ ui/zsh/install.sh
@@ -7,12 +7,5 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # install auto suggestion plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-# install autojump program
-git clone https://github.com/wting/autojump /tmp/autojump
-cd /tmp/autojump
-python install.py
-cd -
-rm -rf /tmp/autojump
 
 rm ~/.zshrc
-
