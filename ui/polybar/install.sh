9076c475d904b2d5ec7fc4f0eac64ff4fa0f05ea
the-c0d3r <the-c0d3r@users.noreply.github.com>
2019-07-15 14:49:17 +0800

    ui/polybar/install.sh: fixed installation

--- ui/polybar/install.sh
+++ ui/polybar/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir -p ~/programming/programs/
+mkdir -p $HOME/programming/programs/
 
 # Install a spotify dependency
-if [ ! -d "~/programming/programs/polybar-spotify" ]; then
+if [ ! -d "$HOME/programming/programs/polybar-spotify" ]; then
     git clone https://github.com/Jvanrhijn/polybar-spotify ~/programming/programs/polybar-spotify
 fi
 
