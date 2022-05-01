70e808e5a5387ac1689fcad3219400c1ce5342ba
the-c0d3r <4526565+the-c0d3r@users.noreply.github.com>
2022-05-01 16:04:38 +0800

    ui/zsh/install.sh: removed 404 git url

--- ui/zsh/install.sh
+++ ui/zsh/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 set -e
+rm ~/.zshrc
+rm -rf ~/.oh-my-zsh
+
 # installs oh-my-zsh plugin manager
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
@@ -10,13 +13,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # install hacker quote
 git clone https://github.com/oldratlee/hacker-quotes.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/hacker-quotes
-# install zsh command architect
-git clone https://github.com/psprint/zsh-cmd-architect.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-cmd-architect
-# install alias-tips
-git clone https://github.com/djui/alias-tips.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/alias-tips
 # install powerlevel 10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-
 
 rm ~/.zshrc
 
