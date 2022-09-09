97d7257bd48122b48d7757ead6427b27cbd9805d
the-c0d3r <4526565+the-c0d3r@users.noreply.github.com>
2022-09-09 10:19:26 +0800

    nvim: fixed config issue

--- files/nvim/custom/chadrc.lua
+++ files/nvim/custom/chadrc.lua
@@ -1,42 +1,73 @@
+
 local M = {}
 
 M.ui = {
-   theme = "onedark",
+  theme = "onedark",
 }
 
 M.plugins = {
   override = {
      ["NvChad/ui"] = {
-       statusline = {
-         separator_style = "arrow", -- default/round/block/arrow
-         overriden_modules = nil,
-       },
+      statusline = {
+        separator_style = "arrow", -- default/round/block/arrow
+        overriden_modules = nil,
+      },
 
-       -- lazyload it when there are 1+ buffers
-       tabufline = {
-         enabled = true,
-         lazyload = false,
-         overriden_modules = nil,
+      -- lazyload it when there are 1+ buffers
+      tabufline = {
+        enabled = true,
+        lazyload = false,
+        overriden_modules = nil,
        },
      },
   },
 
   user = {
-    ["simrat39/symbols-outline.nvim"] = {
-       require("symbols-outline").setup(),
+    -- dashboard
+    ["goolord/alpha-nvim"] = {
+      disable = false,
     },
+    -- key hint prompt
+    ["folke/which-key.nvim"] = {
+      disable = false,
+    },
+    ["williamboman/mason.nvim"] = {
+      ensure_installed = {
+        -- lua stuff
+        "lua-language-server",
+        "yaml-language-server",
+        "python-lsp-server",
+        "pyright",
+        "clangd",
+        "ansible-language-server",
+        "lua-language-server",
+        "stylua",
+
+        -- web dev
+        "css-lsp",
+        "html-lsp",
+        "json-lsp",
+
+        -- shell
+        "shfmt",
+        "shellcheck",
+      },
+    },
+
+    -- outline for symbols
+    ["simrat39/symbols-outline.nvim"] = {
+      config = function()
+        require("symbols-outline").setup()
+      end,
+    },
+    -- LSP configuration
     ["neovim/nvim-lspconfig"] = {
       config = function()
         require "plugins.configs.lspconfig"
         require "custom.plugins.lspconfig"
       end,
     },
-    ["goolord/alpha-nvim"] = {
-       disable = false,
-    },
-    ["folke/which-key.nvim"] = {
-        disable = false,
-    },
+
     -- display marks on the sidebar
     ['kshenoy/vim-signature'] = {},
     -- code alignment gaip+keyword, gaip=, gaip*
@@ -46,10 +77,11 @@ M.plugins = {
     -- multiple cursor location
     -- ['terryma/vim-multiple-cursors'] = {},
     ['easymotion/vim-easymotion'] = {},
-
     -- smooth scrolling
     ['karb94/neoscroll.nvim'] = {
-       require('neoscroll').setup(),
+      config = function()
+        require('neoscroll').setup()
+      end,
     },
   },
 }
