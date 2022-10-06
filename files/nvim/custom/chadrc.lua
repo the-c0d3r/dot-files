268ce65e13b83a964e380589a17377bbee96e5c4
the-c0d3r <4526565+the-c0d3r@users.noreply.github.com>
2022-10-06 15:19:38 +0800

    nvim: reformat to 4 spaces, added tabnine

--- files/nvim/custom/chadrc.lua
+++ files/nvim/custom/chadrc.lua
@@ -1,174 +1,183 @@
 local M = {}
 
 M.ui = {
-  theme = "onedark",
+    theme = "onedark",
 }
 
 M.mappings = {
-  keys = {
-    n = {
-      ["<C-o>"] = { "<cmd> Telescope find_files <CR>", "Open Telescope find files" },
-      ["<C-q>"] = { "<cmd> Telescope live_grep <CR>", "Open Telescope find string" },
-      ["<C-,>"] = { "<cmd> HopLine<CR>", "Hop to Line" }
+    keys = {
+        n = {
+            ["<C-o>"] = { "<cmd> Telescope find_files <CR>", "Open Telescope find files" },
+            ["<C-q>"] = { "<cmd> Telescope live_grep <CR>", "Open Telescope find string" },
+            ["<C-,>"] = { "<cmd> HopLine<CR>", "Hop to Line" }
+        }
     }
-  }
 }
 
 M.plugins = {
-  override = {
-    ["NvChad/ui"] = {
-      statusline = {
-        separator_style = "arrow", -- default/round/block/arrow
-        overriden_modules = nil,
-      },
+    override = {
+        ["NvChad/ui"] = {
+            statusline = {
+                separator_style = "arrow", -- default/round/block/arrow
+                overriden_modules = nil,
+            },
 
-      -- lazyload it when there are 1+ buffers
-      tabufline = {
-        enabled = true,
-        lazyload = false,
-        overriden_modules = nil,
-      },
-    },
+            -- lazyload it when there are 1+ buffers
+            tabufline = {
+                enabled = true,
+                lazyload = false,
+                overriden_modules = nil,
+            },
+        },
 
-    ["williamboman/mason.nvim"] = {
-      ensure_installed = {
-        -- lua stuff
-        "lua-language-server",
-        "yaml-language-server",
-        "python-lsp-server",
-        "pyright",
-        "clangd",
-        "ansible-language-server",
-        "lua-language-server",
-        "stylua",
-        "json-lsp",
-        "html-lsp",
-        "cmake-language-server",
-        "bash-language-server",
+        ["williamboman/mason.nvim"] = {
+            ensure_installed = {
+                -- lua stuff
+                "lua-language-server",
+                "yaml-language-server",
+                "python-lsp-server",
+                "pyright",
+                "clangd",
+                "ansible-language-server",
+                "lua-language-server",
+                "stylua",
+                "json-lsp",
+                "html-lsp",
+                "cmake-language-server",
+                "bash-language-server",
 
-        -- web dev
-        "css-lsp",
-        "html-lsp",
-        "json-lsp",
+                -- web dev
+                "css-lsp",
+                "html-lsp",
+                "json-lsp",
 
-        -- shell
-        "shfmt",
-        "shellcheck",
-      },
-    },
-  },
-
-  user = {
-    -- dashboard
-    ["goolord/alpha-nvim"] = {
-      disable = false,
-    },
-    -- key hint prompt
-    ["folke/which-key.nvim"] = {
-      disable = false,
+                -- shell
+                "shfmt",
+                "shellcheck",
+            },
+        },
     },
 
-    -- outline for symbols
-    ["simrat39/symbols-outline.nvim"] = {
-      config = function()
-        require("symbols-outline").setup()
-      end,
-    },
-    -- LSP configuration
-    ["neovim/nvim-lspconfig"] = {
-      config = function()
-        require "plugins.configs.lspconfig"
-        require "custom.plugins.lspconfig"
-      end,
-    },
+    user = {
+        -- dashboard
+        ["goolord/alpha-nvim"] = {
+            disable = false,
+        },
+        -- key hint prompt
+        ["folke/which-key.nvim"] = {
+            disable = false,
+        },
 
-    -- code alignment gaip+keyword, gaip=, gaip*
-    ['junegunn/vim-easy-align'] = {},
-    -- multiple cursor location
-    -- ['terryma/vim-multiple-cursors'] = {},
-    ['phaazon/hop.nvim'] = {
-      config = function()
-        require('hop').setup()
-      end,
-    },
-    -- smooth scrolling
-    ['karb94/neoscroll.nvim'] = {
-      config = function()
-        require('neoscroll').setup()
-      end,
-    },
+        -- outline for symbols
+        ["simrat39/symbols-outline.nvim"] = {
+            config = function()
+                require("symbols-outline").setup()
+            end,
+        },
+        -- LSP configuration
+        ["neovim/nvim-lspconfig"] = {
+            config = function()
+                require "plugins.configs.lspconfig"
+                require "custom.plugins.lspconfig"
+            end,
+        },
 
-    -- show marks
-    ['chentoast/marks.nvim'] = {
-      config = function()
-        require('marks').setup()
-      end,
-    },
+        -- code alignment gaip+keyword, gaip=, gaip*
+        ['junegunn/vim-easy-align'] = {},
+        -- multiple cursor location
+        -- ['terryma/vim-multiple-cursors'] = {},
+        ['phaazon/hop.nvim'] = {
+            config = function()
+                require('hop').setup()
+            end,
+        },
+        -- smooth scrolling
+        ['karb94/neoscroll.nvim'] = {
+            config = function()
+                require('neoscroll').setup()
+            end,
+        },
 
-    -- cursorline highlight
-    ['RRethy/vim-illuminate'] = {},
+        -- show marks
+        ['chentoast/marks.nvim'] = {
+            config = function()
+                require('marks').setup()
+            end,
+        },
 
-    -- prettify folded code
-    ['anuvyklack/pretty-fold.nvim'] = {
-      config = function()
-        require('pretty-fold').setup()
-      end,
-    },
+        -- cursorline highlight
+        ['RRethy/vim-illuminate'] = {},
 
-    -- fold provider
-    ['kevinhwang91/promise-async'] = {},
-    ['kevinhwang91/nvim-ufo'] = {
-      config = function()
-        require('ufo').setup()
-      end,
-    },
+        -- prettify folded code
+        ['anuvyklack/pretty-fold.nvim'] = {
+            config = function()
+                require('pretty-fold').setup()
+            end,
+        },
 
-    -- diff view
-    ['sindrets/diffview.nvim'] = {
-      requires = "nvim-lua/plenary.nvim",
-      after = 'plenary.nvim',
-      config = function()
-        require('diffview').setup()
-      end,
-    },
+        -- fold provider
+        ['kevinhwang91/promise-async'] = {},
+        ['kevinhwang91/nvim-ufo'] = {
+            config = function()
+                require('ufo').setup()
+            end,
+        },
 
-    -- show diagnostics, references, quick fixes
-    ['folke/trouble.nvim'] = {
-      requires = "kyazdani42/nvim-web-devicons",
-      config = function()
-        require('trouble').setup()
-      end,
-    },
+        -- diff view
+        ['sindrets/diffview.nvim'] = {
+            requires = "nvim-lua/plenary.nvim",
+            after = 'plenary.nvim',
+            config = function()
+                require('diffview').setup()
+            end,
+        },
 
-    -- auto session
-    ['rmagatti/auto-session'] = {
-      config = function()
-        require('auto-session').setup({
-          log_level = "error",
-          auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
-        })
-      end,
-    },
+        -- show diagnostics, references, quick fixes
+        ['folke/trouble.nvim'] = {
+            requires = "kyazdani42/nvim-web-devicons",
+            config = function()
+                require('trouble').setup()
+            end,
+        },
 
-    -- source code analysis and navigation tool
-    ['ray-x/guihua.lua'] = {
-      run = 'cd lua/fzy && make'
+        -- tabnine AI assisted code completion
+        ["tzachar/cmp-tabnine"] = {
+            after = "nvim-cmp",
+            run = "./install.sh",
+            config = function()
+                require "custom.plugins.tabnine"
+            end,
+        },
+
+        -- auto session
+        ['rmagatti/auto-session'] = {
+            config = function()
+                require('auto-session').setup({
+                    log_level = "error",
+                    auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
+                })
+            end,
+        },
+
+        -- source code analysis and navigation tool
+        ['ray-x/guihua.lua'] = {
+            run = 'cd lua/fzy && make'
+        },
+        ['ray-x/navigator.lua'] = {
+            after = "nvim-lspconfig",
+            config = function()
+                require('navigator').setup({
+                    lsp = {
+                        -- need to disable lsp here, as mason will install and handle it
+                        disable_lsp = { 'all' },
+                        -- format on save will cause issues if the code is not compatible, like windows c programs
+                        format_on_save = false,
+                    },
+                    mason = true,
+                })
+            end,
+        },
     },
-    ['ray-x/navigator.lua'] = {
-      after = "nvim-lspconfig",
-      config = function()
-        require('navigator').setup({
-          lsp = {
-            -- need to disable lsp here, as mason will install and handle it
-            disable_lsp = { 'all' },
-            -- format on save will cause issues if the code is not compatible, like windows c programs
-            format_on_save = false,
-          },
-          mason = true,
-        })
-      end,
-    },
-  },
 }
 
 return M
