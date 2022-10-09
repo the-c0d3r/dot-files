1751a2304368cc23a93538d06f1c64c5f638f5a9
the-c0d3r <4526565+the-c0d3r@users.noreply.github.com>
2022-10-09 13:21:53 +0800

    nvim: fixed diffview plugin

--- files/nvim/custom/chadrc.lua
+++ files/nvim/custom/chadrc.lua
@@ -129,8 +129,19 @@ M.plugins = {
 
         -- diff view
         ['sindrets/diffview.nvim'] = {
-            requires = "nvim-lua/plenary.nvim",
+            requires = {
+                { 'nvim-lua/plenary.nvim' },
+                { 'kyazdani42/nvim-web-devicons' }
+            },
             after = 'plenary.nvim',
+            cmd = {
+                'DiffviewOpen',
+                'DiffviewFileHistory',
+                'DiffviewClose',
+                'DiffviewToggleFiles',
+                'DiffviewFocusFiles',
+                'DiffviewRefresh'
+            },
             config = function()
                 require('diffview').setup()
             end,
