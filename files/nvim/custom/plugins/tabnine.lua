99715fd07f315d4483a129dc1218ff2837495fd7
the-c0d3r <4526565+the-c0d3r@users.noreply.github.com>
2022-10-31 14:36:18 +0800

    nvim: moved tabnine to bottom of the suggestion list

--- files/nvim/custom/plugins/tabnine.lua
+++ files/nvim/custom/plugins/tabnine.lua
@@ -37,7 +37,6 @@ cmp.setup {
     sorting = {
         priority_weight = 2,
         comparators = {
-            require('cmp_tabnine.compare'),
             compare.offset,
             compare.exact,
             compare.score,
@@ -46,6 +45,7 @@ cmp.setup {
             compare.sort_text,
             compare.length,
             compare.order,
+            require('cmp_tabnine.compare'),
         },
     },
     experimental = { ghost_text = true }, -- show autocomplete suggestions in grey text
