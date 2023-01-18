44435d0f35bf7d5f330d5888978fde0358f1bef1
the-c0d3r <4526565+the-c0d3r@users.noreply.github.com>
2023-01-18 13:41:03 +0800

    nvim: moved keymappings to chadrc

--- files/nvim/custom/init.lua
+++ files/nvim/custom/init.lua
@@ -1,9 +1,10 @@
 -- fold configuration
 vim.opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 -- vim.opt.foldcolumn = '2'
-vim.opt.foldlevel = 99
+vim.opt.foldlevel = 1
 vim.opt.foldlevelstart = 99
 vim.opt.foldenable = true
+vim.opt.foldnestmax = 1
 
 -- vim tab width to 4 spaces
 vim.opt.tabstop = 4
@@ -14,16 +15,3 @@ vim.opt.shiftwidth = 4
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', extends = '›', precedes = '‹', trail = '·' }
 
--- hop plugin keymapping
-vim.api.nvim_set_keymap('', 'f',
-    "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>"
-    , {})
-vim.api.nvim_set_keymap('', 'F',
-    "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>"
-    , {})
-vim.api.nvim_set_keymap('', 't',
-    "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>"
-    , {})
-vim.api.nvim_set_keymap('', 'T',
-    "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>"
-    , {})
