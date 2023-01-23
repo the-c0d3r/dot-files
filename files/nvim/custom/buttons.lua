8deeeb7cfce43fa18a16d22724c1a30c9ed1d819
the-c0d3r <4526565+the-c0d3r@users.noreply.github.com>
2023-01-23 23:03:22 +0800

    nvim: added forward and backward buttons, remapped keybindings

---
+++ files/nvim/custom/buttons.lua
@@ -0,0 +1,12 @@
+local tabufline_modules = require "nvchad_ui.tabufline.modules"
+
+vim.cmd "function! JumpBackward(a,b,c,d) \n lua vim.cmd(vim.api.nvim_replace_termcodes('normal <C-o>',true,true,true)) \n endfunction"
+vim.cmd "function! JumpForward(a,b,c,d) \n lua vim.cmd(vim.api.nvim_replace_termcodes('normal 1<C-i>',true,true,true)) \n endfunction"
+
+return {
+    buttons = function()
+        local jmp_fwd_btn = "%@JumpForward@%#Test#" .. " › " .. "%X"
+        local jmp_bwd_btn = "%@JumpBackward@%#Test#" .. " ‹ " .. "%X"
+        return jmp_bwd_btn .. jmp_fwd_btn .. tabufline_modules.buttons()
+    end,
+}
