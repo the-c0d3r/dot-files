db19ff411b2c06049c5ed4e9989604e6f784d75b
the-c0d3r <4526565+the-c0d3r@users.noreply.github.com>
2024-11-08 13:27:42 +0800

    nvim: added mouse bindings

--- files/nvim/lua/mappings.lua
+++ files/nvim/lua/mappings.lua
@@ -7,7 +7,6 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-
 -- ctrl keys
 -- ctrl + o and ctrl + i are reserved for default jumplist jumps
 map("n", "<C-q>", "<cmd>Telescope live_grep <cr>")
@@ -23,3 +22,16 @@ map("n", "<leader>fn", "<cmd>enew<cr>")
 map("n", "<leader>gl", "<cmd>LazyGit<cr>")
 map("n", "-", "<cmd>split<cr>")
 map("n", "_", "<cmd>vsplit<cr>")
+
+-- Keyboard users
+map("n", "<C-t>", function()
+  require("menu").open "default"
+end, {})
+
+-- mouse users + nvimtree users!
+map("n", "<RightMouse>", function()
+  vim.cmd.exec '"normal! \\<RightMouse>"'
+
+  local options = vim.bo.ft == "NvimTree" and "nvimtree" or "default"
+  require("menu").open(options, { mouse = true })
+end, {})
