96efe3fad2b950313c065b481a8a3191a4d3551f
the-c0d3r <4526565+the-c0d3r@users.noreply.github.com>
2026-01-05 18:07:05 +0800

    updated starship and tmux config to pastal colour theme

--- programs/tmux.nix
+++ programs/tmux.nix
@@ -26,9 +26,9 @@
           set -g @nova-nerdfonts-left 
           set -g @nova-nerdfonts-right 
           set -g @nova-segment-mode "#{?client_prefix,Ω,ω}"
-          set -g @nova-segment-mode-colors "#50fa7b #282a36"
+          set -g @nova-segment-mode-colors "##ff7da4 #282a36"
           set -g @nova-segment-whoami "#(whoami)@#h"
-          set -g @nova-segment-whoami-colors "#50fa7b #282a36"
+          set -g @nova-segment-whoami-colors "##a1a7fa #282a36"
           set -g @nova-pane "#I#{?pane_in_mode,  #{pane_mode},}  #W"
           set -g @nova-rows 0
           set -g @nova-segments-0-left "mode"
