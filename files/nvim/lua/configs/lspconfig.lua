48d78048cbb84f742437425e27acc1aa17cbacfe
the-c0d3r <4526565+the-c0d3r@users.noreply.github.com>
2026-01-09 15:04:02 +0800

    nvim: updated lsp config

--- files/nvim/lua/configs/lspconfig.lua
+++ files/nvim/lua/configs/lspconfig.lua
@@ -1,8 +1,6 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
 -- EXAMPLE
 local servers = {
   "ansiblels",
@@ -16,13 +14,4 @@ local servers = {
   "pylsp",
   -- "llm_ls",
 }
-local nvlsp = require "nvchad.configs.lspconfig"
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
+vim.lsp.enable(servers)
