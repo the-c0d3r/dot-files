c332307eff1a5a8644401055fdd88ec97c5c6b09
the-c0d3r <4526565+the-c0d3r@users.noreply.github.com>
2026-01-28 12:07:10 +0800

    darwin-configuration.nix: made the borders glow

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -182,7 +182,7 @@
 
   services.jankyborders = {
     enable = true;
-    active_color = "0xffe1e3e4";
+    active_color = "glow(0xffe1e3e4)";
     inactive_color = "0xff494d64";
     width = 5.0;
   };
