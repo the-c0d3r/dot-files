07790ad330e00f2780b5567eff1a2549a6bf4eca
the-c0d3r <4526565+the-c0d3r@users.noreply.github.com>
2026-03-28 19:48:44 +0800

    vscode: added tinymist

--- programs/vscode.nix
+++ programs/vscode.nix
@@ -3,19 +3,29 @@
 { config, pkgs, lib, ... }:
 
 {
+  # Install tinymist (Typst language server) system-wide
+  home.packages = with pkgs; [
+    tinymist
+  ];
+
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
     profiles.default = {
       extensions = with pkgs.vscode-extensions; [
-        vscodevim.vim
+        vscodevim.vim             # vim emulation plugin
+        myriad-dreamin.tinymist   # typst plugin
       ];
       userSettings = {
         "workbench.colorTheme" = "Default Light Modern";
         "workbench.list.openMode" = "doubleClick";
 
+        # claude code configs
         "claudeCode.preferredLocation" = "sidebar";
         "claudeCode.claudeProcessWrapper" = "${pkgs.claude-code}/bin/claude";
+
+        # tinymist configs
+        "tinymist.serverPath" = "${pkgs.tinymist}/bin/tinymist";
 
         "files.autoSave" = "afterDelay";
         "terminal.integrated.stickyScroll.enabled" = false;
