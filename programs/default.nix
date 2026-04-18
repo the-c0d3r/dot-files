28e4ed4db468eddc0fa1c7db467351c5ef29a59d
the-c0d3r <4526565+the-c0d3r@users.noreply.github.com>
2026-04-18 12:46:04 +0800

    removed git from server flag

--- programs/default.nix
+++ programs/default.nix
@@ -4,13 +4,13 @@
   imports = [
     # CLI programs — always included
     ./atuin.nix
-    ./git.nix
     ./neovim.nix
     ./starship.nix
     ./tmux.nix
     ./zsh.nix
   ] ++ lib.optionals (!isServer) [
     # GUI / desktop programs — excluded in server mode
+    ./git.nix    # git has my own config, which shouldn't apply on server.
     ./i3.nix
     ./kitty.nix
     ./obsidian.nix
